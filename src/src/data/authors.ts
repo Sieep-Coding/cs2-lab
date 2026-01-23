c5fff11f002d38e6e8cf8015fb903e3ad39aca79
sieep-coding <nickstambaugh@proton.me>
2026-01-23 11:18:23 -0500

    bio

--- src/src/data/authors.ts
+++ src/src/data/authors.ts
@@ -14,6 +14,6 @@ export const authors: Props[] = [
     name: "Nick Stambaugh",
     slug: "nick-stambaugh",
     image: marioImage,
-    bio: "Nick Stambaugh has been following pro Counter-Strike since 2009, he witnessed the franchise transform from a grassroots scene to a global esports phenomenon. Nick provides high-level analysis on tactics and trends.",
+    bio: "Nick Stambaugh has been following pro Counter-Strike since 2009. He witnessed the franchise transform from a grassroots scene to a global esports phenomenon. Nick provides high-level analysis on tactics and trends.",
   },
 ];
