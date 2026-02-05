c14f6a3097db8c8c4353f134404c6238e62a1858
sieep-coding <nickstambaugh@proton.me>
2026-02-05 17:52:19 -0500

    new danger zone mod

--- src/src/data/authors.ts
+++ src/src/data/authors.ts
@@ -11,9 +11,9 @@ export type Author = Props;
 
 export const authors: Props[] = [
   {
-    name: "Nick Stambaugh",
-    slug: "nick-stambaugh",
+    name: "Nick s",
+    slug: "nick-s",
     image: marioImage,
-    bio: "Nick Stambaugh has been following pro Counter-Strike since 2009. He witnessed the franchise transform from a grassroots scene to a global esports phenomenon. Nick provides high-level analysis on tactics and trends.",
+    bio: "Nick s has been following pro Counter-Strike since 2009. He witnessed the franchise transform from a grassroots scene to a global esports phenomenon. Nick provides high-level analysis on tactics and trends.",
   },
 ];
